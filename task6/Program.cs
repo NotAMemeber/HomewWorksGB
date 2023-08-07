c7c81ff3c0662fc680156f035c777fa2614d4f56
Pandora66 <tolstov047@yandex.ru>
2023-08-07 23:16:02 +0300

    6-я задача

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿string converter;
+int num;
+Console.WriteLine("Введите число");
+converter = Console.ReadLine();
+num = Convert.ToInt32(converter);
+if (num % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
