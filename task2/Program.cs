5fb59a3e2c75e756ff93c6418da95890acf7fca7
Pandora66 <tolstov047@yandex.ru>
2023-08-07 22:44:12 +0300

    2-я задача

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿string converter;
+int a;
+int b;
+Console.WriteLine("Введите a");
+converter = Console.ReadLine();
+a = Convert.ToInt32(converter);
+Console.WriteLine("Введите b");
+converter = Console.ReadLine();
+b = Convert.ToInt32(converter);
+if (a>b)
+{
+Console.WriteLine("Максимальное число - " + a);
+}
+else
+Console.WriteLine("Максимальное число - " + b);
