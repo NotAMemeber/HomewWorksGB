64352f4e8c8a27c39b7b37d494e0b25cd0cdb1a5
Pandora66 <tolstov047@yandex.ru>
2023-08-07 23:09:56 +0300

    4-я задача

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿string converter;
+int lenght = 3;
+int[] nums = new int[lenght];
+Console.WriteLine("Введите a");
+converter = Console.ReadLine();
+nums[0] = Convert.ToInt32(converter);
+int max = nums[0];
+Console.WriteLine("Введите b");
+converter = Console.ReadLine();
+nums[1] = Convert.ToInt32(converter);
+Console.WriteLine("Введите c");
+converter = Console.ReadLine();
+nums[2] = Convert.ToInt32(converter);
+for (int i=1;i < lenght; i++)
+{
+    if (nums[i] > max)
+    {
+        max = nums[i];
+    }
+}
+Console.WriteLine("Max = " + max);
