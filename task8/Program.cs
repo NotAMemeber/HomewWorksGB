3e341cf15dac9fded272b0db2762895ae14e11fd
Pandora66 <tolstov047@yandex.ru>
2023-08-07 23:24:10 +0300

    Добавил 8-ю задачу

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿string converter;
+int N;
+Console.WriteLine("Введите число");
+converter = Console.ReadLine();
+N = Convert.ToInt32(converter);
+while (N>0)
+{
+    if (N % 2 == 0)
+    {
+        Console.Write(N +",");
+    }
+    N--;
+}
