53db9ca554c733f321d288cb0e3ad1d4f5ed0ecd
Pandora66 <tolstov047@yandex.ru>
2023-08-10 22:52:38 +0300

    13-я задача

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿string converter;
+Console.WriteLine("Введите число");
+converter = Console.ReadLine();
+int fullnum = Convert.ToInt32(converter);
+Console.WriteLine(fullnum / 100);
+if ((fullnum / 100) >= 1)
+{
+char num = converter[2];
+Console.WriteLine("Третья цифра - " + num);
+}
+else 
+Console.WriteLine("третьей цифры нет");
