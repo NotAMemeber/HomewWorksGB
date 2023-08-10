ea77271448abf5995848692a38b629a3b96c925a
Pandora66 <tolstov047@yandex.ru>
2023-08-10 23:09:10 +0300

    задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿string converter;
+Console.WriteLine("Введите число");
+converter = Console.ReadLine();
+int day = Convert.ToInt32(converter);
+if ( day > 0 && day < 6)
+{
+    Console.WriteLine("Нет");
+}
+else  if (day < 8 && day >0)
+{
+    Console.WriteLine("Да");
+}
+else
+Console.WriteLine("Не день недели");
