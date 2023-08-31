faa8dab73df3b81623429a7b0115723e0cab60fb
Pandora66 <tolstov047@yandex.ru>
2023-08-31 19:38:36 +0300

    задача 25

---
+++ task25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+double power ()
+{
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    return Math.Pow(number1,number2);
+}
+Console.WriteLine(power());
