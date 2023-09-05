c5ccbf2c70614e411cc8820b8d09b72c2f271332
Pandora66 <tolstov047@yandex.ru>
2023-09-05 18:01:46 +0300

    add task36

---
+++ task36/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void mas(int a)
+{
+for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(-1000,1000);
+Console.Write(randomArray[i] + " ");
+}
+
+}
+
+int summary(int[] randomArray)
+{
+int summary = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (i % 2 != 0)
+summary = summary + randomArray[i];
+}
+return summary;
+}
+
+mas(a);
+Console.Write($"\n Сумма элементов, стоящих на нечетных позициях: {summary(randomArray)}");
